de2a29cae1092cdbf374484de1625b96e478c8bd
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2020-12-25 16:53:52 +0100

    Clean up script and add more logs

--- index.js
+++ index.js
@@ -1,87 +1,94 @@
-let axios = require(`axios`)
-  , extract = require(`extract-zip`)
-  , { createWriteStream, mkdirSync, rmdirSync, unlinkSync } = require(`fs`)
-  , { join } = require(`path`)
-  , notionAPI = `https://www.notion.so/api/v3`
-  , { NOTION_TOKEN, NOTION_SPACE_ID } = process.env
-  , client = axios.create({
-      baseURL: notionAPI,
-      headers: {
-        Cookie: `token_v2=${NOTION_TOKEN}`
-      },
-    })
-  , die = (str) => {
-      console.error(str);
-      process.exit(1);
-    }
-;
+const axios = require(`axios`);
+const extract = require(`extract-zip`);
+const { createWriteStream, mkdirSync, rmdirSync, unlinkSync } = require(`fs`);
+const { join } = require(`path`);
+
+const notionAPI = `https://www.notion.so/api/v3`;
+const { NOTION_TOKEN, NOTION_SPACE_ID } = process.env;
+const client = axios.create({
+  baseURL: notionAPI,
+  headers: {
+    Cookie: `token_v2=${NOTION_TOKEN}`,
+  },
+});
 
 if (!NOTION_TOKEN || !NOTION_SPACE_ID) {
-  die(`Need to have both NOTION_TOKEN and NOTION_SPACE_ID defined in the environment.
-See https://medium.com/@arturburtsev/automated-notion-backups-f6af4edc298d for
-notes on how to get that information.`);
+  console.error(
+    `Environment variable NOTION_TOKEN or NOTION_SPACE_ID is missing. Check the README.md for more information.`
+  );
+  process.exit(1);
 }
 
-async function post (endpoint, data) {
-  return client.post(endpoint, data);
-}
-
-async function sleep (seconds) {
+const sleep = async (seconds) => {
   return new Promise((resolve) => {
     setTimeout(resolve, seconds * 1000);
   });
-}
+};
 
-// formats: markdown, html
-async function exportFromNotion (destination, format) {
-  let { data: { taskId } } = await post(`enqueueTask`, {
-    task: {
-      eventName: `exportSpace`,
-      request: {
-        spaceId: NOTION_SPACE_ID,
-        exportOptions: {
-          exportType: format,
-          timeZone: `Europe/Berlin`,
-          locale: `en`,
-        },
+const round = (number) => Math.round(number * 100) / 100;
+
+const exportFromNotion = async (destination, format) => {
+  const task = {
+    eventName: `exportSpace`,
+    request: {
+      spaceId: NOTION_SPACE_ID,
+      exportOptions: {
+        exportType: format,
+        timeZone: `Europe/Berlin`,
+        locale: `en`,
       },
     },
-  });
-  console.warn(`Enqueued task ${taskId}`);
+  };
+  const {
+    data: { taskId },
+  } = await client.post(`enqueueTask`, { task });
+
+  console.log(`Started Export as task [${taskId}].\n`);
+
   let exportURL;
   while (true) {
     await sleep(2);
-    let { data: { results: tasks } } = await post(`getTasks`, { taskIds: [taskId] })
-      , task = tasks.find(t => t.id === taskId)
-    ;
-    console.warn(`Pages exported: ${task.status.pagesExported}`);
+    const {
+      data: { results: tasks },
+    } = await client.post(`getTasks`, { taskIds: [taskId] });
+    const task = tasks.find((t) => t.id === taskId);
+
+    console.log(`Exported ${task.status.pagesExported} pages.`);
+
     if (task.state === `success`) {
       exportURL = task.status.exportURL;
+      console.log(`\nExport finished.`);
       break;
     }
   }
-  let res = await client({
+
+  const response = await client({
     method: `GET`,
     url: exportURL,
-    responseType: `stream`
+    responseType: `stream`,
   });
-  let stream = res.data.pipe(createWriteStream(destination));
+
+  const size = response.headers["content-length"];
+  console.log(`Downloading ${round(size / 1000 / 1000)}mb...`);
+
+  const stream = response.data.pipe(createWriteStream(destination));
   await new Promise((resolve, reject) => {
     stream.on(`close`, resolve);
     stream.on(`error`, reject);
   });
-}
+};
 
-async function run () {
-  let cwd = process.cwd()
-    , workspaceDir = join(cwd, `workspace`)
-    , workspaceZip = join(cwd, `workspace.zip`)
-  ;
+const run = async () => {
+  const workspaceDir = join(process.cwd(), `workspace`);
+  const workspaceZip = join(process.cwd(), `workspace.zip`);
+
   await exportFromNotion(workspaceZip, `markdown`);
   rmdirSync(workspaceDir, { recursive: true });
   mkdirSync(workspaceDir, { recursive: true });
   await extract(workspaceZip, { dir: workspaceDir });
   unlinkSync(workspaceZip);
-}
+
+  console.log(`✅ Export downloaded and unzipped.`);
+};
 
 run();
