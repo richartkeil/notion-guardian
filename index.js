a7a2f257c4f5af65a881c65339a9e38e61c9a78b
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2023-10-29 12:12:59 +0100

    Use async fs functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require(`axios`);
 const AdmZip = require("adm-zip");
-const { createWriteStream, readdirSync, renameSync, rmdirSync } = require(`fs`);
-const { rm, mkdir, unlink } = require(`fs/promises`);
+const { createWriteStream } = require(`fs`);
+const { rm, mkdir, unlink, readdir, rename, rmdir } = require(`fs/promises`);
 const { join } = require(`path`);
 
 const unofficialNotionAPI = `https://www.notion.so/api/v3`;
@@ -109,7 +109,7 @@ const run = async () => {
   console.log(`✅ Export downloaded and unzipped.`);
 };
 
-function extractZip(filename, destination) {
+async function extractZip(filename, destination) {
   const zip = new AdmZip(filename);
   zip.extractAllTo(destination, /* overwrite */ true);
 
@@ -119,30 +119,36 @@ function extractZip(filename, destination) {
   );
 
   // Extract any "Part-*.zip" files that were found
-  partFiles.forEach((partFile) => {
-    partFile = join(destination, partFile);
-    const partZip = new AdmZip(partFile);
-    partZip.extractAllTo(destination, /* overwrite */ true);
-    unlink(partFile); // Delete the extracted "Part-*.zip" file
-  });
+  await Promise.all(
+    partFiles.map(async (partFile) => {
+      partFile = join(destination, partFile);
+      const partZip = new AdmZip(partFile);
+      partZip.extractAllTo(destination, /* overwrite */ true);
+      await unlink(partFile); // Delete the extracted "Part-*.zip" file
+    })
+  );
 
   // Check if any folders with name starting with "Export-*" were extracted
-  const extractedFolders = readdirSync(destination);
+  const extractedFolders = await readdir(destination);
   const exportFolders = extractedFolders.filter((name) =>
     name.startsWith("Export-")
   );
 
   // Move the contents of any "Export-*" folders to the destination folder
-  exportFolders.forEach((folderName) => {
-    const folderPath = join(destination, folderName);
-    const contents = readdirSync(folderPath);
-    contents.forEach((file) => {
-      const filePath = join(folderPath, file);
-      const newFilePath = join(destination, file);
-      renameSync(filePath, newFilePath);
-    });
-    rmdirSync(folderPath);
-  });
+  await Promise.all(
+    exportFolders.map(async (folderName) => {
+      const folderPath = join(destination, folderName);
+      const contents = await readdir(folderPath);
+      await Promise.all(
+        contents.map(async (file) => {
+          const filePath = join(folderPath, file);
+          const newFilePath = join(destination, file);
+          await rename(filePath, newFilePath);
+        })
+      );
+      await rmdir(folderPath);
+    })
+  );
 }
 
 run();
